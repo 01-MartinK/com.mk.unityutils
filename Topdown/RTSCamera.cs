564766e1d50c07b567d543b1c2642cd68c474955
Martin K <nitram1509@gmail.com>
2025-03-22 17:19:51 +0200

    Refactored the RTSCamera to Unity 6

--- Topdown/RTSCamera.cs
+++ Topdown/RTSCamera.cs
@@ -1,9 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class RTSCamera : MonoBehaviour
 {
+    [Header("Input")]
+    public InputAction Movement;
+    public InputAction Rotation;
+
     [Header("Movement")]
     public float speed;
     public float rotationSpeed;
@@ -11,21 +16,40 @@ public class RTSCamera : MonoBehaviour
 
     private Vector2 input;
     private Vector3 updatedPosition;
+    private float updatedRotation;
+
+    private void OnEnable()
+    {
+        Movement.Enable();
+        Rotation.Enable();
+    }
+
+    private void OnDisable()
+    {
+        Movement.Disable();
+        Rotation.Disable();
+    }
+
+    private void Awake()
+    {
+        updatedPosition = transform.position;
+    }
 
     private void Update()
     {
         GetInput();
         transform.position = Vector3.Lerp(transform.position, updatedPosition, lerpSpeed * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.Euler(0, updatedRotation, 0), lerpSpeed * Time.deltaTime);
     }
 
     private void FixedUpdate()
     {
-        updatedPosition += new Vector3(input.x, 0, input.y) * speed;
+        updatedPosition += (transform.forward * input.y + transform.right * input.x) * speed;
     }
 
     private void GetInput()
     {
-        input.x = InputManager.Movement.Horizontal.ReadValue<float>();
-        input.y = InputManager.Movement.Vertical.ReadValue<float>();
+        input = Movement.ReadValue<Vector2>();
+        updatedRotation -= Rotation.ReadValue<float>() * rotationSpeed;
     }
 }
